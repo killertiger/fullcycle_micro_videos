5ac690dc3fc9adc27d48acc14db3577bd82b846d
moliveira <moliveira@pinterest.com>
2023-05-05 12:42:25 +0000

    Add id in Category

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
-
-@dataclass
+import uuid
+@dataclass(kw_only=True)
 class Category:
+    id: uuid.UUID = field(default_factory=lambda: uuid.uuid4())
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
