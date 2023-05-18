bd02712b9187e6ef1e3db180c0ad98f6923f9aad
moliveira <moliveira@pinterest.com>
2023-05-18 11:04:37 +0000

    test: :white_check_mark: Add validators tests

--- src/__seedwork/tests/unit/domain/test_unit_validators.py
+++ src/__seedwork/tests/unit/domain/test_unit_validators.py
@@ -25,7 +25,7 @@ class TestValidatorRules(unittest.TestCase):
                 ValidatorRules(item['value'], item['prop']).required()
 
             self.assertEqual('The prop is required',
-                             assert_error.exception.args[0])
+                             assert_error.exception.args[0],)
 
         valid_data = [
             {'value': 'test', 'prop': 'prop'},
@@ -37,3 +37,85 @@ class TestValidatorRules(unittest.TestCase):
         for item in valid_data:
             self.assertIsInstance(ValidatorRules.values(
                 item['value'], item['prop']).required(), ValidatorRules)
+
+    def test_string_rule(self):
+        invalid_data = [
+            {'value': 5, 'prop': 'prop'},
+            {'value': True, 'prop': 'prop'},
+            {'value': {}, 'prop': 'prop'},
+        ]
+
+        for item in invalid_data:
+            msg = f'value: {item["value"]}, prop: {item["prop"]}'
+            with self.assertRaises(ValidationException, msg=msg) as assert_error:
+                ValidatorRules(item['value'], item['prop']).string()
+
+            self.assertEqual(
+                'The prop must be a string',
+                assert_error.exception.args[0],
+            )
+
+        valid_data = [
+            {'value': None, 'prop': 'prop'},
+            {'value': "", 'prop': 'prop'},
+            {'value': 'some value', 'prop': 'prop'},
+        ]
+
+        for item in valid_data:
+            self.assertIsInstance(ValidatorRules(
+                item['value'], item['prop']).string(), ValidatorRules)
+
+    def test_max_rule(self):
+        invalid_data = [
+            {'value': "t" * 5, 'prop': 'prop'}
+        ]
+
+        for item in invalid_data:
+            msg = f'value: {item["value"]}, prop: {item["prop"]}'
+            with self.assertRaises(ValidationException, msg=msg) as assert_error:
+                ValidatorRules(item['value'], item['prop']).max_length(4)
+
+            self.assertEqual(
+                'The prop must be less than 4 characters',
+                assert_error.exception.args[0],
+            )
+
+        valid_data = [
+            {'value': None, 'prop': 'prop'},
+            {'value': "t" * 5, 'prop': 'prop'},
+        ]
+
+        for item in valid_data:
+            self.assertIsInstance(
+                ValidatorRules(item['value'], item['prop']).max_length(5),
+                ValidatorRules,
+            )
+
+    def test_boolean_rule(self):
+        invalid_data = [
+            {'value': "", 'prop': 'prop'},
+            {'value': 5, 'prop': 'prop'},
+            {'value': {}, 'prop': 'prop'},
+        ]
+        
+        for item in invalid_data:
+            msg = f'value: {item["value"]}, prop: {item["prop"]}'
+            with self.assertRaises(ValidationException, msg=msg) as assert_error:
+                ValidatorRules(item['value'], item['prop']).boolean()
+                
+            self.assertEquals(
+                'The prop must be a boolean',
+                assert_error.exception.args[0]
+            )
+        
+        valid_data = [
+            {'value': None, 'prop': 'prop'},
+            {'value': True, 'prop': 'prop'},
+            {'value': False, 'prop': 'prop'},
+        ]
+        
+        for item in valid_data:
+            self.assertIsInstance(
+                ValidatorRules(item['value'], item['prop']).boolean(),
+                ValidatorRules
+            )
