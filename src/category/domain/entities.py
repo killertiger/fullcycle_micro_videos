68ee9b6444b2fda3ce98dde9da42c283abfeab21
moliveira <moliveira@pinterest.com>
2023-05-19 01:07:18 +0000

    feat: :sparkles: Add validation in Category entity with unit tests

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 from typing import Optional
 
 from __seedwork.domain.entities import Entity
+from __seedwork.domain.validators import ValidatorRules
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -13,12 +14,27 @@ class Category(Entity):
     created_at: Optional[datetime] = field(
         default_factory=lambda: datetime.now())
 
+    def __new__(cls, **kwargs):
+        cls.validate(
+            name = kwargs.get('name'),
+            description = kwargs.get('description'),
+            is_active = kwargs.get('is_active'),
+        )
+        return super(Category, cls).__new__(cls)
+
     def update(self, name: str, description: str):
+        self.validate(name, description)
         self._set('name', name)
         self._set('description', description)
-        
+
     def activate(self):
         self._set('is_active', True)
-    
+
     def deactivate(self):
         self._set('is_active', False)
+
+    @classmethod
+    def validate(cls, name: str, description: str, is_active: bool = None):
+        ValidatorRules.values(name, 'name').required().string().max_length(255)
+        ValidatorRules.values(description, 'description').string()
+        ValidatorRules.values(is_active, 'is_active').boolean()
