6d6af01e879eaf379d7a23e03bec9afb9be71d99
moliveira <moliveira@pinterest.com>
2023-07-06 01:25:25 +0000

    feat: :sparkles: Add CategoryInMemoryRepository with tests

--- src/category/domain/repositories.py
+++ src/category/domain/repositories.py
@@ -1,8 +1,37 @@
 from abc import ABC
+from typing import List, Any
 
-from __seedwork.domain.repositories import RepositoryInterface
+from __seedwork.domain.repositories import (
+    InMemorySearchableRepository,
+    SearchableRepositoryInterface,
+    SearchParams as DefaultSearchParams,
+    SearchResult as DefaultSearchResult
+)
 from category.domain.entities import Category
 
 
-class CategoryRepository(RepositoryInterface[Category], ABC):
+class _SearchParams(DefaultSearchParams):
     pass
+
+
+class _SearchResult(DefaultSearchResult):
+    pass
+
+class CategoryRepository(SearchableRepositoryInterface[Category, _SearchParams, _SearchResult], ABC):
+    SearchParams = _SearchParams
+    SearchResult = _SearchResult
+
+
+class CategoryInMemoryRepository(CategoryRepository, InMemorySearchableRepository):
+    sortable_fields: List[str] = ['created_at', 'name']    
+    
+    def _apply_filter(self, items: List[Category], filter_param: Any | None) -> List[Category]:
+        if filter_param:
+            items = filter(lambda i: filter_param.lower() in i.name.lower(), items)
+        return list(items)
+    
+    def _apply_sort(self, items: List, sort: str | None, sort_dir: str | None) -> List:
+        if not sort:
+            sort = 'created_at'
+            
+        return super()._apply_sort(items, sort, sort_dir)
--- src/category/infa/repositories.py
+++ src/category/infa/repositories.py
@@ -1,6 +1,6 @@
-from category.domain.repositories import CategoryRepository
-from __seedwork.domain.repositories import InMemoryRepository
+# from category.domain.repositories import CategoryRepository
+# from __seedwork.domain.repositories import InMemoryRepository
 
 
-class CategoryInMemoryRepository(CategoryRepository, InMemoryRepository):
-    pass
+# class CategoryInMemoryRepository(CategoryRepository, InMemoryRepository):
+#     pass
