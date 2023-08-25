f4e0654cce05b69396818ecb140f8f48d821559f
moliveira <moliveira@pinterest.com>
2023-08-25 11:38:31 +0000

    Create category project and run

--- src/core/category/infra/django/models.py
+++ src/core/category/infra/django/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from core.__seedwork.domain.entities import Entity
+
+print(Entity)
 
 # Create your models here.
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=255)
