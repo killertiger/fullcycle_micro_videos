ef0265edf65bb203681c9e338a01f110afe84497
moliveira <moliveira@pinterest.com>
2023-08-28 13:14:36 +0000

    First api for category

--- src/core/category/infra/django/api.py
+++ src/core/category/infra/django/api.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from core.category.application.use_cases import CreateCategoryUseCase
+from core.category.infra.in_memory.repositories import CategoryInMemoryRepository
+from dataclasses import asdict
 
-@api_view()
+@api_view(['POST'])
 def hello_world(request: Request):
-    return Response({'message': 'API is working'})
+    create_use_case = CreateCategoryUseCase(CategoryInMemoryRepository())
+    input_param = CreateCategoryUseCase.Input(name=request.data['name'])
+    output = create_use_case.execute(input_param)
+    return Response(asdict(output))
