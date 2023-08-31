3f1ab30fbabafa63709760d196fd923a5bfb7546
moliveira <moliveira@pinterest.com>
2023-08-31 11:37:12 +0000

    test: :white_check_mark: CategoryResource Get

--- src/core/category/tests/unit/infra/django/test_unit_api.py
+++ src/core/category/tests/unit/infra/django/test_unit_api.py
@@ -7,13 +7,14 @@ from rest_framework.request import Request
 from core.category.application.use_cases import (
     CreateCategoryUseCase,
     ListCategoriesUseCase,
+    GetCategoryUseCase,
 )
 from core.category.infra.django.api import CategoryResource
 
 
 class TestCategoryResourceUnit(unittest.TestCase):
     def __init_all_none(self):
-        return {"list_use_case": None, "create_use_case": None}
+        return {"list_use_case": None, "create_use_case": None, "get_use_case": None}
 
     def test_post_method(self):
         send_data = {"name": "fake name"}
@@ -99,12 +100,91 @@ class TestCategoryResourceUnit(unittest.TestCase):
                         "name": "Movie",
                         "description": None,
                         "is_active": True,
-                        "created_at": mock_list_use_case.execute.return_value.items[0].created_at,
+                        "created_at": mock_list_use_case.execute.return_value.items[
+                            0
+                        ].created_at,
                     },
                 ],
                 "total": 1,
                 "current_page": 1,
                 "per_page": 2,
-                "last_page": 1
+                "last_page": 1,
+            },
+        )
+
+    def test_if_get_invoke_get_object(self):
+        mock_get_use_case = mock.Mock(GetCategoryUseCase)
+        mock_list_use_case = mock.Mock(ListCategoriesUseCase)
+
+        mock_get_use_case.execute.return_value = GetCategoryUseCase.Output(
+            id="fc98cf57-4615-4b0a-b5eb-373870ca27ce",
+            name="Movie",
+            description=None,
+            is_active=True,
+            created_at=datetime.now(),
+        )
+
+        resource = CategoryResource(
+            **{
+                **self.__init_all_none(),
+                "get_use_case": lambda: mock_get_use_case,
+                "list_use_case": lambda: mock_list_use_case,
+            }
+        )
+
+        response = resource.get(None, "fc98cf57-4615-4b0a-b5eb-373870ca27ce")
+
+        self.assertEqual(mock_list_use_case.call_count, 0)
+        mock_get_use_case.execute.assert_called_with(
+            GetCategoryUseCase.Input(id="fc98cf57-4615-4b0a-b5eb-373870ca27ce")
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data,
+            {
+                "id": "fc98cf57-4615-4b0a-b5eb-373870ca27ce",
+                "name": "Movie",
+                "description": None,
+                "is_active": True,
+                "created_at": mock_get_use_case.execute.return_value.created_at,
+            },
+        )
+        
+    
+    def test_if_get_invoke_get_object_2(self):
+        resource = CategoryResource(**self.__init_all_none())
+        resource.get_object = mock.Mock()
+        resource.get(None, "fc98cf57-4615-4b0a-b5eb-373870ca27ce")
+        resource.get_object.assert_called_once()
+
+    def test_get_method(self):
+        mock_get_use_case = mock.Mock(GetCategoryUseCase)
+
+        mock_get_use_case.execute.return_value = GetCategoryUseCase.Output(
+            id="fc98cf57-4615-4b0a-b5eb-373870ca27ce",
+            name="Movie",
+            description=None,
+            is_active=True,
+            created_at=datetime.now(),
+        )
+
+        resource = CategoryResource(
+            **{**self.__init_all_none(), "get_use_case": lambda: mock_get_use_case}
+        )
+
+        response = resource.get_object("fc98cf57-4615-4b0a-b5eb-373870ca27ce")
+
+        mock_get_use_case.execute.assert_called_with(
+            GetCategoryUseCase.Input(id="fc98cf57-4615-4b0a-b5eb-373870ca27ce")
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data,
+            {
+                "id": "fc98cf57-4615-4b0a-b5eb-373870ca27ce",
+                "name": "Movie",
+                "description": None,
+                "is_active": True,
+                "created_at": mock_get_use_case.execute.return_value.created_at,
             },
         )
