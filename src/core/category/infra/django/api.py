92fe18577ad93f56d87c1684cf70cf5c1d231eba
moliveira <moliveira@pinterest.com>
2023-08-31 01:51:07 +0000

    improve TestCategoryResourceUnit

--- src/core/category/infra/django/api.py
+++ src/core/category/infra/django/api.py
@@ -3,6 +3,7 @@ from dataclasses import asdict, dataclass
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
+from rest_framework import status as http_status
 from core.category.application.use_cases import (CreateCategoryUseCase, 
                                                  ListCategoriesUseCase
                                                  )
@@ -16,9 +17,9 @@ class CategoryResource(APIView):
     
     def post(self, request: Request):
         print(request.data)
-        input_param = CreateCategoryUseCase.Input(name=request.data['name'])
+        input_param = CreateCategoryUseCase.Input(**request.data)
         output = self.create_use_case().execute(input_param)
-        return Response(asdict(output))
+        return Response(asdict(output), http_status.HTTP_201_CREATED)
     
     def get(self, request: Request):
         print(request.data)
--- src/core/category/tests/unit/infra/django/test_unit_api.py
+++ src/core/category/tests/unit/infra/django/test_unit_api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 from unittest import mock
 from core.category.application.use_cases import (
@@ -9,19 +10,36 @@ from rest_framework.request import Request
 
 class TestCategoryResourceUnit(unittest.TestCase):
     def test_post_method(self):
-        create_use_case = mock.Mock(CreateCategoryUseCase)
+        send_data = {'name': 'fake name'}
+        mock_create_use_case = mock.Mock(CreateCategoryUseCase)
+        
+        mock_create_use_case.execute.return_value = CreateCategoryUseCase.Output(
+            id='fc98cf57-4615-4b0a-b5eb-373870ca27ce',
+            name='Movie',
+            description=None,
+            is_active=True,
+            created_at=datetime.now()
+        )
         
         resource = CategoryResource(
             list_use_case=None,
-            create_use_case=lambda: create_use_case
+            create_use_case=lambda: mock_create_use_case
         )
         
-        send_data = {'name': 'fake name'}
         _request = APIRequestFactory().post('', send_data)
         request = Request(_request)
         request._full_data = send_data
         
-        try:
-            response = resource.post(request)
-        except:
-            create_use_case.execute.assert_called()
+        response = resource.post(request)
+        
+        mock_create_use_case.execute.assert_called_with(CreateCategoryUseCase.Input(
+            name='fake name'
+        ))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, {
+            'id': 'fc98cf57-4615-4b0a-b5eb-373870ca27ce',
+            'name':'Movie',
+            'description': None,
+            'is_active': True,
+            'created_at': mock_create_use_case.execute.return_value.created_at
+        })
