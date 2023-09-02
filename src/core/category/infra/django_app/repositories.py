517e6f566fb4042028d9890300b67c3839905417
moliveira <moliveira@pinterest.com>
2023-09-02 13:53:02 +0000

    feat: :sparkles: CategoryDjangoRepository find_by_id

--- src/core/category/infra/django_app/repositories.py
+++ src/core/category/infra/django_app/repositories.py
@@ -1,4 +1,5 @@
 from typing import List
+from core.__seedwork.domain.exceptions import NotFoundException
 from core.__seedwork.domain.value_objects import UniqueEntityId
 from core.category.domain.entities import Category
 from core.category.domain.repositories import CategoryRepository
@@ -10,7 +11,15 @@ class CategoryDjangoRepository(CategoryRepository):
         CategoryModel.objects.create(**entity.to_dict())
 
     def find_by_id(self, entity_id: str | UniqueEntityId) -> Category:
-        raise NotImplementedError()
+        id_str = str(entity_id)
+        model = self._get(id_str)
+        return Category(
+            unique_entity_id=UniqueEntityId(id_str),
+            name=model.name,
+            description=model.description,
+            is_active=model.is_active,
+            created_at=model.created_at,
+        )
 
     def find_all(self) -> List[Category]:
         raise NotImplementedError()
@@ -20,6 +29,14 @@ class CategoryDjangoRepository(CategoryRepository):
 
     def delete(self, entity_id: str | UniqueEntityId) -> None:
         raise NotImplementedError()
+
+    def _get(self, entity_id: str) -> CategoryModel:
+        try:
+            CategoryModel.objects.get(pk=entity_id)
+        except CategoryModel.DoesNotExist as exception:
+            raise NotFoundException(
+                f"Entity not found using ID '{entity_id}'"
+            ) from exception
 
     def search(
         self, input_params: CategoryRepository.SearchParams
