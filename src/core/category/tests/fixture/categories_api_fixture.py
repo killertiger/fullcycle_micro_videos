f07399946de78ef33b0a9e9f05a7ee1537193156
moliveira <moliveira@pinterest.com>
2023-09-18 12:30:57 +0000

    test: :white_check_mark: test fixture and TestCategoryResourcePostMethodInt improvements

---
+++ src/core/category/tests/fixture/categories_api_fixture.py
@@ -0,0 +1,37 @@
+from typing import Any
+from dataclasses import dataclass
+
+
+@dataclass
+class Request:
+    body: Any
+
+
+@dataclass
+class Response:
+    body: Any
+
+
+@dataclass
+class HttpExpect:
+    request: Request
+    response: Response
+
+
+class CategoryAPIFixture:
+    @staticmethod
+    def keys_in_category_response():
+        return ['id', 'name', 'description', 'is_active', 'created_at']
+
+    @staticmethod
+    def arrange_for_save():
+        return [
+            HttpExpect(
+                request=Request(body={'name': 'Movie',}),
+                response=Response(body={'description': None, 'is_active': True}),
+            ),
+            HttpExpect(
+                request=Request(body={'name': 'Movie', 'description': 'some description'}),
+                response=Response(body={'is_active': True}),
+            )
+        ]
--- src/core/category/tests/integration/infra/django_app/api/test_int_post.py
+++ src/core/category/tests/integration/infra/django_app/api/test_int_post.py
@@ -1,23 +1,53 @@
 import pytest
+from rest_framework.test import APIRequestFactory
+from rest_framework.request import Request
 from core.category.domain.repositories import CategoryRepository
 from core.category.infra.django_app.api import CategoryResource
 from core.category.infra.django_app.models import CategoryModel
 from core.category.infra.django_app.repositories import CategoryDjangoRepository
-
+from core.category.tests.fixture.categories_api_fixture import (
+    CategoryAPIFixture,
+    HttpExpect,
+)
+from django_app import container
 
 @pytest.mark.django_db
 class TestCategoryResourcePostMethodInt:
     resource: CategoryResource
     repo: CategoryRepository
-    
+
     @classmethod
     def setup_class(cls):
-        cls.repo = CategoryDjangoRepository()
+        # cls.repo = CategoryDjangoRepository()
+        cls.repo = container.repository_category_in_memory()
         cls.resource = CategoryResource(
-            create_use_case=None,
+            create_use_case=container.use_case_category_create_category,
             update_use_case=None,
             get_use_case=None,
             list_use_case=None,
             delete_use_case=None,
         )
+
+    @pytest.mark.parametrize('http_expect', CategoryAPIFixture.arrange_for_save())
+    def test_method_post(self, http_expect: HttpExpect):
+        request_factory = APIRequestFactory()
+        _request = request_factory.get('/categories')
+        request = Request(_request) 
+        request._full_data = http_expect.request.body
+        response = self.resource.post(request)
+        assert response.status_code == 201
+        assert CategoryAPIFixture.keys_in_category_response() == list(response.data.keys())
         
+        category_created = self.repo.find_by_id(response.data['id'])
+        assert response.data == {
+            'id': category_created.id,
+            'name': category_created.name,
+            'description': category_created.description,
+            'is_active': category_created.is_active,
+            'created_at': category_created.created_at
+        }
+        
+        expected_data = {**http_expect.request.body, **http_expect.response.body}
+        for key, value in expected_data.items():
+            assert response.data[key] == value
+            
