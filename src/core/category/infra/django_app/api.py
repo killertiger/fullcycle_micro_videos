89e5a04ad20e440df9f266b37c71249e6e65c825
moliveira <moliveira@pinterest.com>
2023-09-19 11:46:35 +0000

    refactor: :recycle: Presenter Layer using Serializer

--- src/core/category/infra/django_app/api.py
+++ src/core/category/infra/django_app/api.py
@@ -1,10 +1,11 @@
 from typing import Callable
 from dataclasses import asdict, dataclass
-from core.category.infra.django_app.serializer import CategorySerializer
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework import status as http_status
+from core.category.application.dto import CategoryOutput
+from core.category.infra.django_app.serializer import CategorySerializer
 from core.category.application.use_cases import (
     CreateCategoryUseCase,
     ListCategoriesUseCase,
@@ -28,7 +29,9 @@ class CategoryResource(APIView):
 
         input_param = CreateCategoryUseCase.Input(**serializer.validated_data)
         output = self.create_use_case().execute(input_param)
-        return Response(asdict(output), http_status.HTTP_201_CREATED)
+        body = CategoryResource.category_to_response(output)
+        
+        return Response(body, http_status.HTTP_201_CREATED)
 
     def get(self, request: Request, id: str = None):
         if id:
@@ -41,7 +44,10 @@ class CategoryResource(APIView):
     def get_object(self, id: str):
         input_param = GetCategoryUseCase.Input(id)
         output = self.get_use_case().execute(input_param)
-        return Response(asdict(output))
+        body = CategoryResource.category_to_response(output)
+        
+        return Response(body, http_status.HTTP_201_CREATED)
+
 
     def put(self, request: Request, id: str):
         serializer = CategorySerializer(data=request.data)
@@ -51,11 +57,18 @@ class CategoryResource(APIView):
             **{'id': id, **serializer.validated_data}
         )
         output = self.update_use_case().execute(input_param)
+        body = CategoryResource.category_to_response(output)
+        
+        return Response(body, http_status.HTTP_201_CREATED)
 
-        return Response(asdict(output))
 
     def delete(self, _request: Request, id: str):
         input_param = DeleteCategoryUseCase.Input(id=id)
         self.delete_use_case().execute(input_param)
 
         return Response(status=http_status.HTTP_204_NO_CONTENT)
+    
+    @staticmethod
+    def category_to_response(category: CategoryOutput):
+        serializer = CategorySerializer(instance= category)
+        return serializer.data
--- src/core/category/infra/django_app/serializer.py
+++ src/core/category/infra/django_app/serializer.py
@@ -1,7 +1,9 @@
-from rest_framework import serializers
+from rest_framework import serializers, ISO_8601
 
 
 class CategorySerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField(required=False, allow_null=True)
     is_active = serializers.BooleanField(required=False)
+    created_at = serializers.DateTimeField(read_only=True, format=ISO_8601)
