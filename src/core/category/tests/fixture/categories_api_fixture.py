ca79c7ceff71d781ed9bf5df00c6b4edde3c9974
moliveira <moliveira@pinterest.com>
2023-09-20 12:14:15 +0000

    refactor: :white_check_mark: Improve categories_api_fixture to name the test

--- src/core/category/tests/fixture/categories_api_fixture.py
+++ src/core/category/tests/fixture/categories_api_fixture.py
@@ -1,5 +1,6 @@
 from typing import Any
 from dataclasses import dataclass
+import pytest
 
 
 @dataclass
@@ -25,13 +26,21 @@ class CategoryAPIFixture:
 
     @staticmethod
     def arrange_for_save():
-        return [
+        data = [
             HttpExpect(
-                request=Request(body={'name': 'Movie',}),
+                request=Request(
+                    body={
+                        'name': 'Movie',
+                    }
+                ),
                 response=Response(body={'description': None, 'is_active': True}),
             ),
             HttpExpect(
-                request=Request(body={'name': 'Movie', 'description': 'some description'}),
+                request=Request(
+                    body={'name': 'Movie', 'description': 'some description'}
+                ),
                 response=Response(body={'is_active': True}),
-            )
+            ),
         ]
+
+        return [pytest.param(item, id=str(item.request.body)) for item in data]
