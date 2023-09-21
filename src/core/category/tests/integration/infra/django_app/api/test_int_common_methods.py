4de87e1a3ec1855cd741304ad61bf018f319f864
moliveira <moliveira@pinterest.com>
2023-09-21 10:05:03 +0000

    test: :white_check_mark: add TestCategoryResourceCommonMethodsInt

---
+++ src/core/category/tests/integration/infra/django_app/api/test_int_common_methods.py
@@ -0,0 +1,31 @@
+from django.utils import timezone
+from core.category.application.dto import CategoryOutput
+from core.category.infra.django_app.api import CategoryResource
+from core.category.tests.helpers import init_category_resource_all_none
+
+class TestCategoryResourceCommonMethodsInt:
+    
+    category_resource: CategoryResource
+    
+    @classmethod
+    def setup_class(cls):
+        cls.category_resource = CategoryResource(
+            **init_category_resource_all_none()
+        )
+        
+    def test_category_to_response(self):
+        output = CategoryOutput(
+            id='fake id',
+            name='category test',
+            description='description test',
+            is_active=True,
+            created_at=timezone.now()
+        )
+        data = CategoryResource.category_to_response(output)
+        assert data == {
+            'id': 'fake id',
+            'name': 'category test',
+            'description': 'description test',
+            'is_active': True,
+            'created_at': f'{output.created_at.isoformat()[:-6]}Z'
+        }
