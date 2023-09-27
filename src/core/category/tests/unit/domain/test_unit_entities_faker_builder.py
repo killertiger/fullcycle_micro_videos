534c7ca70e5b948b559213e37f86c416609baed7
moliveira <moliveira@pinterest.com>
2023-09-27 12:08:52 +0000

    test: :white_check_mark: Add invalid tests to TestEntitiesFakerBuilder

--- src/core/category/tests/unit/domain/test_unit_entities_faker_builder.py
+++ src/core/category/tests/unit/domain/test_unit_entities_faker_builder.py
@@ -16,33 +16,78 @@ class TestEntitiesFakerBuilder(unittest.TestCase):
         unique_entity_id = UniqueEntityId()
         
         faker = CategoryFakerBuilder.a_category()
-        faker.with_unique_entity_id(unique_entity_id)
+        this = faker.with_unique_entity_id(unique_entity_id)
         
         self.assertEqual(faker.unique_entity_id, unique_entity_id)
+        self.assertIsInstance(this, CategoryFakerBuilder)
         
     def test_name_prop(self):
         faker = CategoryFakerBuilder.a_category()
         self.assertIsInstance(faker.name, str)
         
-        faker.with_name('name test')
+        this = faker.with_name('name test')
         self.assertEqual(faker.name, 'name test')
+        self.assertIsInstance(this, CategoryFakerBuilder)
+        
+    def test_invalid_cases_for_name_prop(self):
+        faker = CategoryFakerBuilder.a_category()
+        
+        name_value = faker.with_invalid_name_none().name
+        self.assertIsNone(name_value)
+        
+        name_value = faker.with_invalid_name_empty().name
+        self.assertEqual(name_value, '')
+        
+        name_value = faker.with_invalid_name_not_string().name
+        self.assertEqual(name_value, 123)
+        
+        name_value = faker.with_invalid_name_not_string(10).name
+        self.assertEqual(name_value, 10)
+        
+        name_value = faker.with_invalid_name_too_long().name
+        self.assertEqual(len(name_value), 256)
+        
+    def test_invalid_cases_for_description_prop(self):
+        faker = CategoryFakerBuilder.a_category()
+        
+        this = faker.with_invalid_description_not_string()
+        self.assertIsInstance(this, CategoryFakerBuilder)
+        self.assertEqual(this.description, 123)
+        
+    def test_invalid_cases_for_is_active(self):
+        faker = CategoryFakerBuilder.a_category()
+        
+        this = faker.with_invalid_is_active_none()
+        self.assertIsInstance(this, CategoryFakerBuilder)
+        self.assertIsNone(this.is_active)
+        
+        this = faker.with_invalid_is_active_empty()
+        self.assertIsInstance(this, CategoryFakerBuilder)
+        self.assertEqual(this.is_active, '')
+        
+        this = faker.with_invalid_is_active_not_boolean()
+        self.assertIsInstance(this, CategoryFakerBuilder)
+        self.assertEqual(this.is_active, 123)
     
     def test_description_prop(self):
         faker = CategoryFakerBuilder.a_category()
         self.assertIsInstance(faker.description, str)
         
-        faker.with_name('description test')
+        this = faker.with_description('description test')
         self.assertEqual(faker.description, 'description test')
+        self.assertIsInstance(this, CategoryFakerBuilder)
         
     def test_is_active_prop(self):
         faker = CategoryFakerBuilder.a_category()
         self.assertTrue(faker.is_active)
         
-        faker.deactivate()
+        this = faker.deactivate()
         self.assertFalse(faker.is_active)
+        self.assertIsInstance(this, CategoryFakerBuilder)
         
-        faker.activate()
+        this = faker.activate()
         self.assertTrue(faker.is_active)
+        self.assertIsInstance(this, CategoryFakerBuilder)
     
     def test_created_at_throw_exception_when_is_none(self):
         with self.assertRaises(Exception) as assert_exception:
@@ -54,9 +99,10 @@ class TestEntitiesFakerBuilder(unittest.TestCase):
         created_at = datetime.now()
         
         faker = CategoryFakerBuilder.a_category()
-        faker.with_created_at(created_at)
+        this = faker.with_created_at(created_at)
         
         self.assertEqual(faker.created_at, created_at)
+        self.assertIsInstance(this, CategoryFakerBuilder)
     
     def test_build(self):
         # tmp = CategoryFakerBuilder.a_category()
