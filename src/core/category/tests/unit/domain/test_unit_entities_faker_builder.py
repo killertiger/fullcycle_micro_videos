bccb89c98f72033642cf48dcd29bd2fe881e8905
moliveira <moliveira@pinterest.com>
2023-09-27 11:45:11 +0000

    test: :white_check_mark: Add TestEntitiesFakerBuilder

--- src/core/category/tests/unit/domain/test_unit_entities_faker_builder.py
+++ src/core/category/tests/unit/domain/test_unit_entities_faker_builder.py
@@ -6,6 +6,58 @@ from core.category.domain.entities_faker_builder import CategoryFakerBuilder
 
 class TestEntitiesFakerBuilder(unittest.TestCase):
     
+    def test_unique_entity_id_throw_exception_when_is_none(self):
+        with self.assertRaises(Exception) as assert_exception:
+            faker = CategoryFakerBuilder.a_category()
+            faker.unique_entity_id
+        self.assertEqual(assert_exception.exception.args[0], 'Prop unique_entity_id does not have a factory, use "with methods"')
+    
+    def test_unique_entity_id_prop(self):
+        unique_entity_id = UniqueEntityId()
+        
+        faker = CategoryFakerBuilder.a_category()
+        faker.with_unique_entity_id(unique_entity_id)
+        
+        self.assertEqual(faker.unique_entity_id, unique_entity_id)
+        
+    def test_name_prop(self):
+        faker = CategoryFakerBuilder.a_category()
+        self.assertIsInstance(faker.name, str)
+        
+        faker.with_name('name test')
+        self.assertEqual(faker.name, 'name test')
+    
+    def test_description_prop(self):
+        faker = CategoryFakerBuilder.a_category()
+        self.assertIsInstance(faker.description, str)
+        
+        faker.with_name('description test')
+        self.assertEqual(faker.description, 'description test')
+        
+    def test_is_active_prop(self):
+        faker = CategoryFakerBuilder.a_category()
+        self.assertTrue(faker.is_active)
+        
+        faker.deactivate()
+        self.assertFalse(faker.is_active)
+        
+        faker.activate()
+        self.assertTrue(faker.is_active)
+    
+    def test_created_at_throw_exception_when_is_none(self):
+        with self.assertRaises(Exception) as assert_exception:
+            faker = CategoryFakerBuilder.a_category()
+            faker.created_at
+        self.assertEqual(assert_exception.exception.args[0], 'Prop created_at does not have a factory, use "with methods"')
+        
+    def test_created_at_prop(self):
+        created_at = datetime.now()
+        
+        faker = CategoryFakerBuilder.a_category()
+        faker.with_created_at(created_at)
+        
+        self.assertEqual(faker.created_at, created_at)
+    
     def test_build(self):
         # tmp = CategoryFakerBuilder.a_category()
         # print(CategoryFakerBuilder.a_category().build())
