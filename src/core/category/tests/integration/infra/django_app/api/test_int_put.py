ffd8e78f5c4bd6b4357b398d1ab6a00d1b64daac
killertiger <killertiger@gmail.com>
2023-10-03 12:55:52 +0000

    test: :white_check_mark: Improve TestCategoryResourcePutMethodInt

--- src/core/category/tests/integration/infra/django_app/api/test_int_put.py
+++ src/core/category/tests/integration/infra/django_app/api/test_int_put.py
@@ -1,10 +1,12 @@
-from core.category.domain.entities import Category
-from core.category.infra.django_app.serializer import CategorySerializer
+from unittest.mock import patch, PropertyMock
 import pytest
+from django_app import container
 from rest_framework.test import APIRequestFactory
 from rest_framework.request import Request
-from django_app import container
-from unittest.mock import patch, PropertyMock
+from rest_framework.exceptions import ErrorDetail, ValidationError
+from core.category.domain.entities import Category
+from core.category.infra.django_app.serializer import CategorySerializer
+from core.__seedwork.domain.exceptions import NotFoundException
 from core.category.domain.repositories import CategoryRepository
 from core.category.infra.django_app.api import CategoryResource
 from core.category.tests.fixture.categories_api_fixture import (
@@ -67,6 +69,23 @@ class TestCategoryResourcePutMethodInt:
             mock_is_valid.assert_called()
             mock_validated_data.assert_called()
             assert assert_exception.value.error == http_expect.exception.error
+
+    def test_throw_exception_when_uuid_is_valid(self):
+        request = make_request(http_method='put')
+        
+        with pytest.raises(ValidationError) as assert_exception:
+            self.resource.put(request, 'fake id')
+            
+        assert assert_exception.value.detail == {
+            'id': [ErrorDetail(string='Must be a valid UUID.', code='invalid')]
+        }
+        
+    def test_throw_exception_when_category_not_found(self):
+        uuid_value = 'ab368028-9fc3-4eae-810c-3735af62d6f2'
+        request = make_request(http_method='put', send_data={'name': 'test'})
+        with pytest.raises(NotFoundException) as assert_exception:
+            self.resource.put(request, id=uuid_value)
+        assert assert_exception.value.args[0] == f"Entity not found using ID '{uuid_value}'"
 
     @pytest.mark.parametrize('http_expect', UpdateCategoryAPIFixture.arrange_for_save())
     def test_method_put(self, http_expect: HttpExpect):
