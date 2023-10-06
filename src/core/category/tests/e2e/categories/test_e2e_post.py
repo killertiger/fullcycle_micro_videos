a161327763ff6fffc74f10850ae60adcf7b3b8d7
killertiger <killertiger@gmail.com>
2023-10-06 11:34:15 +0000

    test: :white_check_mark: First E2E test TestCategoriesPostE2E

---
+++ src/core/category/tests/e2e/categories/test_e2e_post.py
@@ -0,0 +1,12 @@
+import pytest
+from rest_framework.test import APIClient
+from rest_framework.response import Response
+
+
+@pytest.mark.django_db
+class TestCategoriesPostE2E:
+    def test_post(self):
+        client_http = APIClient()
+        response: Response = client_http.post('/categories/', data={'name': 'test'})
+        assert response.status_code == 201
+        
